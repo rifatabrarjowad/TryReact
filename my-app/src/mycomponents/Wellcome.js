fc59c02d463652c73961f7642c1b0c139b1fcef3
rifatabrarjowad <rajdiprifat2004@gmail.com>
2021-09-30 23:45:54 +0600

    bootstrap

--- my-app/src/mycomponents/Wellcome.js
+++ my-app/src/mycomponents/Wellcome.js
@@ -1,8 +1,15 @@
 import React,{Component} from 'react'
 
 class Wellcome extends Component{
+    DoThisF(a){
+        alert(a)
+    }
     render(){
-        return <h1>Hello I am {this.props.name}. I am {this.props.age}</h1>
+
+        return <div>
+            <h1>Hello I am {this.props.name}. I am {this.props.age}</h1>
+            <button onClick={this.DoThisF.bind(this,'I am from class')}> Alert</button>
+        </div>
     }
 }
 export default Wellcome;
