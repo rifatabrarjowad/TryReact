72bd99180531f904170f2fd5585fe36d6017f68d
rifatabrarjowad <rajdiprifat2004@gmail.com>
2021-10-02 23:34:12 +0600

    create a ghori

---
+++ file03/lws/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import Clock from './compo/Clock'
+
+function App() {
+  return (
+    <Clock locale="bn-BD" ></Clock>
+  );
+}
+
+export default App;
