475878b881c6760006fff85bfc62083e2f38a15d
rifatabrarjowad <rajdiprifat2004@gmail.com>
2021-10-03 18:14:07 +0600

    adding file

--- file03/lws/src/compo/Clock.js
+++ file03/lws/src/compo/Clock.js
@@ -1,16 +1,20 @@
 import React from 'react'
 class Clock extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { date: new Date() };
-    }
+
+    state = { date: new Date() };
+
     componentDidMount() {
-        setInterval(() => {
-            this.setState({
-                date: new Date()
-            })
-        }, 1000);
+        this.clockTimer = setInterval(() => this.tick(), 1000);
     }
+    componentWillUnmount() {
+        clearInterval(this.clockTimer)
+    }
+    tick() {
+        this.setState({
+            date: new Date()
+        });
+    }
+
     render() {
         return (
             <h1 className="heading" >
