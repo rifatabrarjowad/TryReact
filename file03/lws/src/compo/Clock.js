7874cbc2b2de1b19cf1b844d21323797181ed1fe
rifatabrarjowad <rajdiprifat2004@gmail.com>
2021-10-06 23:47:01 +0600

    clock js

--- file03/lws/src/compo/Clock.js
+++ file03/lws/src/compo/Clock.js
@@ -1,27 +1,43 @@
-import React from 'react'
-class Clock extends React.Component {
+import React from 'react';
+import Button from './Button';
 
-    state = { date: new Date() };
+class Clock extends React.Component {
+    state = { date: new Date(), locale: 'bn-BD' };
 
     componentDidMount() {
         this.clockTimer = setInterval(() => this.tick(), 1000);
     }
+
     componentWillUnmount() {
-        clearInterval(this.clockTimer)
+        clearInterval(this.clockTimer);
     }
+
+    handleClick = (locale) => {
+        this.setState({
+            locale,
+        });
+    };
+
     tick() {
         this.setState({
-            date: new Date()
+            date: new Date(),
         });
     }
 
     render() {
+        console.log('clock component rendered');
+        const { date, locale } = this.state;
         return (
-            <h1 className="heading" >
-                <span className="text" >{this.state.date.toLocaleTimeString(this.props.locale)}</span>
-            </h1>
-        )
+            <div>
+                <h1 className="heading">
+                    <span className="text">{date.toLocaleTimeString(locale)}</span>
+                </h1>
+                <Button change={this.handleClick} locale="en-US">
+                    Click here
+                </Button>
+            </div>
+        );
     }
 }
+
 export default Clock;
-// ReactDOM.render(<clock local="bn-BD/"></clock>, document.getElementById('root'));
