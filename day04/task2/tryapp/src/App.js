6394709ee84adc7302fde354443d5ea5500d4d4a
rifatabrajowad <rajdiprifat2004@gmail.com>
2022-02-21 13:14:06 +0600

    error solve

--- day04/task2/tryapp/src/App.js
+++ day04/task2/tryapp/src/App.js
@@ -1,21 +1,23 @@
+import React from "react";
 import "./App.css";
 import Form from "./Form.js";
 import Home from "./Home.js";
 import About from "./About.js";
 
-import { BrowserRouter, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Link to="">Home</Link>
+        <Link to="/">Home</Link>
         <Link to="/about">About</Link>
         <Link to="/form">Form</Link>
-
-        <Route path="" component={Home} />
-        <Route path="/about" component={About} />
-        <Route path="/form" component={Form} />
+        <Routes>
+          <Route path="" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/form" element={<Form />} />
+        </Routes>
       </BrowserRouter>
     </div>
   );
