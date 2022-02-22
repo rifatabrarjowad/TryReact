dbc5c312fbe247b5815840b44da151c57a819d5e
rifatabrajowad <rajdiprifat2004@gmail.com>
2022-02-22 17:08:36 +0600

    update

---
+++ day05/tryapp/src/details/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+export default class About extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <h1 className="text-3xl font-bold ">About</h1>
+      </div>
+    );
+  }
+}
