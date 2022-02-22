d7e4900ef995436f9506f3a91baf167027729a3c
rifatabrajowad <rajdiprifat2004@gmail.com>
2022-02-22 18:00:04 +0600

    file name update and carete click page

--- day05/tryapp/src/App.js
+++ day05/tryapp/src/App.js
@@ -10,10 +10,10 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <nav class="flex items-center justify-between flex-wrap bg-teal-500 p-6">
-          <div class="flex items-center flex-shrink-0 text-white mr-6">
+        <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-6">
+          <div className="flex items-center flex-shrink-0 text-white mr-6">
             <svg
-              class="fill-current h-8 w-8 mr-2"
+              className="fill-current h-8 w-8 mr-2"
               width="54"
               height="54"
               viewBox="0 0 54 54"
@@ -21,14 +21,14 @@ function App() {
             >
               <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" />
             </svg>
-            <span class="font-semibold text-xl tracking-tight">
+            <span className="font-semibold text-xl tracking-tight">
               Tailwind CSS
             </span>
           </div>
-          <div class="block lg:hidden">
-            <button class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+          <div className="block lg:hidden">
+            <button className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
               <svg
-                class="fill-current h-3 w-3"
+                className="fill-current h-3 w-3"
                 viewBox="0 0 20 20"
                 xmlns="http://www.w3.org/2000/svg"
               >
@@ -37,31 +37,32 @@ function App() {
               </svg>
             </button>
           </div>
-          <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-            <div class="text-sm lg:flex-grow">
+          <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+            <div className="text-sm lg:flex-grow">
               <a
                 href="#responsive-header"
-                class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
+                className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
               >
-                Docs
+                <Link to="/">home</Link>
               </a>
               <a
                 href="#responsive-header"
-                class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
+                className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
               >
-                Examples
+                <Link to="/form">Form</Link>
               </a>
-              <a
-                href="#responsive-header"
-                class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white"
+
+              <Link
+                className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white"
+                to="/about"
               >
-                Blog
-              </a>
+                About
+              </Link>
             </div>
             <div>
               <a
-                href="#"
-                class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"
+                href="http://www.facebook.com/rifatabrarjowad"
+                className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"
               >
                 Download
               </a>
