793dce88e05b4f5b21b3b49f0cd2d76b28849d83
rifatabrajowad <rajdiprifat2004@gmail.com>
2022-11-04 04:57:26 +0600

    making server side render for api in next js

--- day06/next_app/pages/post/readpost.tsx
+++ day06/next_app/pages/post/readpost.tsx
@@ -1,11 +1,25 @@
 import Link from "next/link"
-export default function ReadPost() {
+export default function ReadPost({ post }) {
     return (
         <div>
-            <h1>Read Post </h1>
-            <p>Dolore eos sadipscing labore lorem et tempor sadipscing, amet erat voluptua diam at sea consetetur. Et dolor kasd dolores dolor,.</p>
+            <h1>{post.title} </h1>
+            <p>{post.body}</p>
             <hr/>
             <Link href={`/post/allpost`}>Back to All Post</Link>
         </div>
     )
 };
+export async function getStaticProps() {
+    /*
+    If it will the server side render then it need as well
+    const res =  fetch('https://jsonplaceholder.typicode.com/posts/1')
+    [and also the function name be changed to getServerSideProps]
+    
+    */
+    const post = {title: 'Post By', body: 'Dolore eos sadipscing labore lorem et tempor sadipscing, amet erat voluptua diam at sea consetetur. Et dolor kasd dolores dolor,'}
+    return {
+        props: {
+           post
+        },
+    }
+}
